3fae11f0b4eccef4a227d1f88ea2ca876df993a1
mthdht <mthdht@gmail.com>
2023-06-06 09:51:27 +0200

    install leaflet and tailwind

---
+++ src/components/Map.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="map" class="h-screen"></div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  var map = L.map('map').setView([51.505, -0.09], 13);
+});
+</script>
+
+<style scoped></style>
